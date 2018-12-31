fec1d1f9947ba46b3b03086aeb9e4f305266cc50
Daniel Evans <daniel.evans.tech@outlook.com>
2018-12-31 14:30:40 -0800

    Saving

--- Testing.py
+++ Testing.py
@@ -1,6 +1,4 @@
-file = open('Test.txt')
-while True:
-    char = file.read(1)
-    if not char: break
-    print(char)
-for char in (open('Test.txt').read()): print(char)
+test = 'length'
+print(len(test))
+testRange = range(len(test))
+list(testRange)
