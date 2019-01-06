eff69bcab39d49de1a4ca9f03af19fe6297979a6
Daniel Evans <daniel.evans.tech@outlook.com>
2019-01-05 16:56:58 -0800

    1052019

--- Testing.py
+++ Testing.py
@@ -1,3 +1,5 @@
-x = 'spam'
-for i in x:
-    print(i,'Python')
+f = open('Main.py')
+for line in f:
+    findMe = 'cidrDict'
+    x = line.__contains__(findMe)
+    print(x,'found in this line',line)
