724ee6d22ade0ad64641518b94fd8cae152e10c6
Daniel Evans <daniel.evans.tech@outlook.com>
2019-01-08 13:43:46 -0800

    Commit

--- Main.py
+++ Main.py
@@ -1,16 +1,9 @@
-#Test to see if this works right
-test = 'test'
 cidrDict = {
 	'24':'255.255.255.0',
 	'16':'255.255.0.0',
 	'8':'255.0.0.0'
 }
 x = 0
-ipAddr = input('What is the ip address?')
-while x == 0:
-	cidr = input('What is the CIDR?')
-	if cidr in cidrDict:
-		print('Your CIDR has been found, continuing...')
-		x += 1
-	else:
-		print('----------\nCIDR not found, try again\n----------')
+ipAddr = input('What is the starting IP address of your range?')
+cidr = input('What is the CIDR of the network you want to scan?')
+netMask = cidrDict[str(cidr)]
